a514521b840154f723579e9e53bb779ef609550e
AlexAegis <alexaegis@gmail.com>
2016-09-15 10:46:09 +0200

    added

--- lesson01/task01/helloworld.java
+++ lesson01/task01/helloworld.java
@@ -1,5 +1,10 @@
 class helloworld {
-      public static void main(String[] args) {
-            System.out.println("Hello World!");
-      }
+
+    private helloworld() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
