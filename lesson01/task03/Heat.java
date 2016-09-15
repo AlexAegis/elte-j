57c885839ca7c91cc48e1fb8894c2475be3266c8
AlexAegis <alexaegis@gmail.com>
2016-09-15 11:44:05 +0200

    Messages added

--- lesson01/task03/Heat.java
+++ lesson01/task03/Heat.java
@@ -9,7 +9,9 @@ class Heat {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.print("Enter value: ");
         int value = scn.nextInt(); scn.nextLine();
+        System.out.print("Enter heat type: ");
         String type = scn.nextLine();
         try {
             System.out.println(new HeatConverter(value).setHeatType(type).getAsOther());
