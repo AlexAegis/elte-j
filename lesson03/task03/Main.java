976bc766b4b234dca2c0560195962bf5baf4781e
AlexAegis <alexaegis@gmail.com>
2016-09-29 11:44:01 +0200

    task03;

--- lesson03/task03/Main.java
+++ lesson03/task03/Main.java
@@ -1,6 +1,18 @@
+import utils.Sodoku;
+
 public class Main {
 
 	public static void main(String[] args) {
+		byte[][] b = new byte[][]{
+			{1,1,1},
+			{2,2,2},
+			{3,3,3}
+		};
 
+		System.out.println(Sodoku.show(b));
+
+		if(Sodoku.check(b)) {
+			System.out.println("Checked");
+		}
 	}
 }
--- lesson03/task03/utils/Sodoku.java
+++ lesson03/task03/utils/Sodoku.java
@@ -2,6 +2,26 @@ package utils;
 
 public class Sodoku {
 
+	public static boolean check(byte[][] square) {
+		boolean result = true;
+		result &= (square.length == 3);
+		for(byte[] row : square) {
+			result &= (row.length == 3);
+			for(byte element : row) result &= element < 10 && element > 0;
+		}
+		return result;
+	}
 
+	public static String show(byte[][] square) {
+		String result = "";
+		for(byte[] row : square) {
+			for(byte element : row) {
+				result += element;
+				result += " ";
+			}
+			result += "\n";
+		}
+		return result;
+	}
 
 }
