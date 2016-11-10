406ce4035b631ca3a93a8b63e8ceef472bda3a9f
AlexAegis <alexaegis@gmail.com>
2016-11-10 11:16:24 +0100

    lesson 8

--- lesson08/task02/Time.java
+++ lesson08/task02/Time.java
@@ -1,4 +1,5 @@
 public class Time {
+
 	public final int hour;
 	public final int minute;
 	public final int second;
@@ -7,6 +8,18 @@ public class Time {
 		this.hour = hour;
 		this.minute = minute;
 		this.second = second;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(o == null || o.getClass() != this.getClass()) return false;
+		Time time = (Time) o;
+		return this.hour == time.hour && this.minute == time.minute && this.second == time.second; 
+	}
+
+	@Override
+	public int hashCode() {
+		return new Double(hour * 29 + minute * 30 + second * 31).hashCode();
 	}
 
 	@Override
