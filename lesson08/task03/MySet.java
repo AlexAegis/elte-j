ab1df823e4b4ae357b6e8e58b3032c0f5111fbe4
AlexAegis <alexaegis@gmail.com>
2016-11-10 11:38:04 +0100

    lesson 8

---
+++ lesson08/task03/MySet.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class MySet<T> {
+
+	private final static int TABLE_SIZE = 32;
+	private ArrayList<T>[] store;
+
+	public MySet() {
+		strore = new ArrayList[TABLE_SIZE];
+	}
+
+	public void add( T t) {
+
+	}
+
+	public T[] toArray() {
+		return null;
+	}
+}
