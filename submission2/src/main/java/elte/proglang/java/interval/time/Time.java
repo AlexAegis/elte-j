b742a0a37a8056db5bc31d1b43b730abe46da4a7
AlexAegis <alexaegis@gmail.com>
2016-11-30 22:48:31 +0100

    Time class done

--- submission2/src/main/java/elte/proglang/java/interval/time/Time.java
+++ submission2/src/main/java/elte/proglang/java/interval/time/Time.java
@@ -1,4 +1,49 @@
 package elte.proglang.java.interval.time;
 
-public class Time {
+public class Time implements Comparable {
+
+    private int hour;
+    private int minute;
+
+    private Time(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public static Time make(int hour, int minute) {
+        if(hour < 8 || hour > 18 || minute < 0 || minute > 59) return null;
+        else return new Time(hour, minute);
+    }
+
+    public Time add(int minutes) {
+        if(minutes < 0) return null;
+        else return make(hour + minutes / 60, minute + minutes % 60);
+    }
+
+    @Override
+    public String toString() {
+        if(minute < 10) return hour + ":0" + minute;
+        else return hour + ":" + minute;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        else if(o == null || getClass() != o.getClass()) return false;
+        return hour == ((Time) o).hour && minute == ((Time) o).minute;
+    }
+
+    @Override
+    public int hashCode() {
+        return hour + minute;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(o == null || getClass() != o.getClass()) return 0;
+        if(((Time) o).hour > hour || ((Time) o).hour == hour && ((Time) o).minute > minute) return -1;
+        else if(((Time) o).hour < hour || ((Time) o).hour == hour && ((Time) o).minute < minute) return 1;
+        else return 0;
+    }
+
 }
--- submission2/src/test/java/elte/proglang/java/MainTest.java
+++ submission2/src/test/java/elte/proglang/java/MainTest.java
@@ -1,5 +1,6 @@
 package elte.proglang.java;
 
+import elte.proglang.java.interval.time.Time;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
@@ -7,6 +8,11 @@ public class MainTest {
 
     @Test
     public void MainTest() {
+        Time time = Time.make(9, 5);
+        System.out.println(time.toString());
+        System.out.println(time.add(50).toString());
+        System.out.println(time.add(90).toString());
+        System.out.println(time.add(190).toString());
         assertTrue(true);
     }
 }
