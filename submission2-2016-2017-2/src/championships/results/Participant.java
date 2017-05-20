2636104dbe9cb08b41c599a68d25fe2a665525c3
AlexAegis <alexaegis@gmail.com>
2017-05-20 15:35:08 +0200

    Participant interface done

--- submission2-2016-2017-2/src/championships/results/Participant.java
+++ submission2-2016-2017-2/src/championships/results/Participant.java
@@ -1,7 +1,28 @@
 package championships.results;
 
 /**
- * Created by alexa on 2017-05-20.
+ * A világbajnokság egy résztvevőjét ennek az interfésznek a megvalósításai ábrázolják.
+ * Egy résztvevőt alapvetően a neve és a nemzetisége határoz meg, ezek
+ * mindegyike egy String, és egyik sem lehet null vagy 0 hosszúságú (üres) String.
+ *
+ * Tipp: nem megfelelő nevű és/vagy nem megfelelő nemzetiségű résztvevőt eleve nem szabad létrehoznunk.
  */
 public interface Participant {
+
+    /**
+     * Visszaadja ennek a résztvevőnek a nevét.
+     * A név biztosan nem lehet null vagy egy 0 hosszúságú (üres) String.
+     *
+     * @return a résztvevő neve
+     */
+    String getName();
+
+    /**
+     * Visszaadja ennek a résztvevőnek a nemzetiségét.
+     * A nemzetiség biztosan nem lehet null vagy egy 0 hosszúságú (üres) String.
+     *
+     * @return a résztvevő nemzetisége
+     */
+    String getNation();
+
 }
